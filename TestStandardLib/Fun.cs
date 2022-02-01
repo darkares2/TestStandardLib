53eb25bc795e7f6c8f9ee53d56ae9e50433e82f8
Thomas Andersen <thad@demant.com>
2022-02-01 10:24:43 +0100

    Still trying

--- TestStandardLib/Fun.cs
+++ TestStandardLib/Fun.cs
@@ -6,7 +6,11 @@ namespace TestStandardLib
     {
         public int Return10()
         {
-            return 10;
+            Random random = new Random();
+            int value = random.Next(100);
+            value += 45;
+            value -= random.Next(45);
+            return value;
         }
     }
 }
